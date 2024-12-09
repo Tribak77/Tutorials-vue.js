d3430f4a83d69c1670c81cfe0c7f2f6b4694804f
Ayoub Tribak <atribak34@gmail.com>
2024-12-09 23:15:55 +0100

    create a simple to do list app

--- my-first-vue-app/src/App.vue
+++ my-first-vue-app/src/App.vue
@@ -1,30 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>To-Do List</h1>
+    <input v-model="newTask" placeholder="Add a task" />
+    <button @click="addTask">Add</button>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <span :style="{ textDecoration: task.done ? 'line-through' : 'none' }">
+          {{ task.text }}
+        </span>
+        <button @click="toggleTask(index)">Toggle</button>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+  export default {
+    data() {
+      return {
+        newTask: "",
+        tasks: [],
+      };
+    },
+    methods: {
+      addTask() {
+        if (this.newTask.trim()) {
+          this.tasks.push({ text: this.newTask, done: false });
+          this.newTask = "";
+        }
+      },
+      toggleTask(index) {
+        this.tasks[index].done = !this.tasks[index].done;
+      },
+      deleteTask(index) {
+        this.tasks.splice(index, 1);
+      },
+    },
+  };
+</script>
